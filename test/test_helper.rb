932f7a803823cc5b361cc28dc7df584a2c2d66a9
Chad Wilken <chad.wilken@gmail.com>
2023-02-08 11:00:33 -0600

    Update call to attribute_changed? to be Ruby 3 compatible

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,6 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-require 'simplecov-rcov'
 require "codeclimate-test-reporter"
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [
-    SimpleCov::Formatter::HTMLFormatter,
-    SimpleCov::Formatter::RcovFormatter,
-    CodeClimate::TestReporter::Formatter
-  ]
-)
-
-SimpleCov.start do
-  add_filter 'test'
-end
 
 CodeClimate::TestReporter.start
 
